e13c25ab9a7a3824eedd3b14bd8d027f12fad458
maciejholodniuk <maciekholo@gmail.com>
2023-03-14 18:05:46 +0100

    add dataset class

--- jackson/jackson.py
+++ jackson/jackson.py
@@ -9,22 +9,25 @@ class Task:
 
 @dataclass
 class Dataset:
-    id: int
+    id: str
     tasks: list[Task]
 
-def sort_by_release_time(tasks: list[Task]) -> list[Task]:
-    return sorted(tasks, key=lambda task: task.release_time)
 
-def calculate_total_execution_time(tasks: list[Task]) -> int:
-    sorted_tasks = sort_by_release_time(tasks)
+def calculate_total_execution_time(task_dataset: Dataset) -> tuple[str, int]:
+    sorted_tasks = sort_by_release_time(task_dataset.tasks)
     moments_of_task_termination = []
     for idx, task in enumerate(sorted_tasks):
         time = np.max((moments_of_task_termination[idx-1] if idx > 0 else 0, task.release_time)) + task.execution_time
         moments_of_task_termination.append(time)
 
-    return moments_of_task_termination[-1]
+    return (task_dataset.id, moments_of_task_termination[-1])
 
-def read_data_from_files(path_str: str=__file__) -> list[list[tuple[int, int]]]:
+def sort_by_release_time(tasks: list[Task]) -> list[Task]:
+    return sorted(tasks, key=lambda task: task.release_time)
+
+
+
+def read_data_from_files(path_str: str) -> list[Dataset]:
     files = [item.absolute() for item in Path(path_str).iterdir() if item.is_file()]
     data = []
     for file in files:
@@ -34,7 +37,7 @@ def read_data_from_files(path_str: str=__file__) -> list[list[tuple[int, int]]]:
                 if len(line.split()) != 2: continue
                 nums = line.split()
                 current_file_data.append((int(nums[0]), int(nums[1])))
-            data.append(current_file_data)
+        data.append(Dataset(file.name, create_tasks_from_tuple_list(current_file_data)))
 
     return data
 
@@ -42,8 +45,5 @@ def create_tasks_from_tuple_list(task_list: list[tuple[int, int]]) -> list[Task]
     return [Task(raw_task_data[0], raw_task_data[1]) for raw_task_data in task_list]
 
 
-
-
-for raw_tasks in read_data_from_files('./data'):
-    tasks = create_tasks_from_tuple_list(raw_tasks)
+for tasks in read_data_from_files('./data'):
     print(calculate_total_execution_time(tasks))
